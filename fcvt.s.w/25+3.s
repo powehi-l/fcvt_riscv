78d93a6b6aa58fcf823b5b0373feb185fa7e820e
pluto <3200102126@1119272440@qq.com>
2022-10-31 20:40:55 +0800

    init the resposiry

---
+++ fcvt.s.w/25+3.s
@@ -0,0 +1,15 @@
+main:
+        addi    sp,sp,-32
+        sw      s0,28(sp)
+        addi    s0,sp,32
+        li      a5,33554435
+        addi    a5,a5,1
+        sw      a5,-20(s0)
+        lw      a5,-20(s0)
+        fcvt.s.w        fa5,a5
+        fsw     fa5,-24(s0)
+        li      a5,0
+        mv      a0,a5
+        lw      s0,28(sp)
+        addi    sp,sp,32
+        jr      ra
---
+++ fcvt.w.s/dyn/dyn_1.75.s
@@ -0,0 +1,18 @@
+main:
+        addi    sp,sp,-32
+        sw      s0,28(sp)
+        addi    s0,sp,32
+        lui     a5,%hi(.LC0)
+        flw     fa5,%lo(.LC0)(a5)
+        fsw     fa5,-20(s0)
+        flw     fa5,-20(s0)
+        fcvt.w.s a5,fa5,dyn
+        sw      a5,-24(s0)
+        lw      a5,-24(s0)
+        mv      a0,a5
+        lw      s0,28(sp)
+        addi    sp,sp,32
+        jr      ra
+.data
+.LC0:
+        .word   0x3FE00000
